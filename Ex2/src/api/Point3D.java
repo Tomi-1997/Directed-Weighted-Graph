a2bcdbd2ff8263ab6bae340f10ad062e996873d7
bfwontplaywithme <bfwontplaywithme@gmail.com>
2021-12-04 18:42:02 +0200

    P0int3D & vertex class

---
+++ Ex2/src/api/Point3D.java
@@ -0,0 +1,52 @@
+package api;
+
+public class Point3D implements GeoLocation {
+    public double x;
+    public double y;
+    public double z;
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point3D() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public double x() {
+
+        return this.x;
+    }
+
+    @Override
+    public double y() {
+        return this.y;
+    }
+
+    @Override
+    public double z() {
+        return this.z;
+    }
+
+    @Override
+    public double distance(GeoLocation g) {
+        double dist = Math.pow((this.x-g.x()),2) + Math.pow((this.y-g.y()),2) + Math.pow((this.z-g.z()),2);
+        return Math.sqrt(dist);
+    }
+}
