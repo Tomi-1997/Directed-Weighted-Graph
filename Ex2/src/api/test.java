6c096716e9ba2cdfebd3e0a83d7a23c98ddc037e
tomas <tomtom.inbar@gmail.com>
2021-12-05 00:45:02 +0200

    Made a test class for Edge, testing source, info and weight functions.

--- Ex2/src/api/test.java
+++ Ex2/src/api/test.java
@@ -8,8 +8,12 @@ public class test {
     {
         HashMap<String , Vertex> h = new HashMap();
         h.put("0" , new Vertex( new Point3D(1,1,0) , 0));
-        System.out.println(h.size());
-        System.out.println(h.get("0").getInfo());
-        System.out.println(h.containsKey("1"));
+//        System.out.println(h.size());
+//        System.out.println(h.get("0").getInfo());
+//        System.out.println(h.containsKey("1"));
+
+        double phi = (1 + Math.sqrt(5)) / 2;
+        System.out.println(phi);
+        System.out.println(1/phi);
     }
 }
