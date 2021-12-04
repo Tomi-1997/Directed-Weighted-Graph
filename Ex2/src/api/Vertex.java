5b9e31f8dae1b72d1e4044d7db548b476cf639ee
bfwontplaywithme <bfwontplaywithme@gmail.com>
2021-12-04 19:48:19 +0200

    vertex class, vertex test class

--- Ex2/src/api/Vertex.java
+++ Ex2/src/api/Vertex.java
@@ -1,5 +1,64 @@
 package api;
 
-public class Vertex {
+public class Vertex implements NodeData {
+    public Point3D pos;
+    public int id;
+    public double weight;
+    public String info;
+    // tag can be white = 0,black = 2,grey = 1
+    public int tag;
 
+    public Vertex(Point3D pos, int id) {
+        this.pos = pos;
+        this.id = id;
+        this.weight = 0;
+        this.info = this.pos.x+"," +this.pos.y+","+this.pos.z+ "," + this.id;
+        this.tag = 0;
+    }
+
+    @Override
+    public int getKey() {
+        return this.id;
+    }
+
+    @Override
+    public GeoLocation getLocation() {
+        if(this.pos == null) return null;
+        return this.pos;
+    }
+
+    @Override
+    public void setLocation(GeoLocation p) {
+        this.pos = (Point3D)(p);
+    }
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        this.weight = w;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
 }
