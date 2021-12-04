88d507978b4f97adeeda8296df043f1627ba55aa
bfwontplaywithme <bfwontplaywithme@gmail.com>
2021-12-04 20:13:53 +0200

    testing hashmap

---
+++ Ex2/src/api/test.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.util.HashMap;
+
+public class test {
+    /** test class just for testing hashmap **/
+    public static void main(String[]args)
+    {
+        HashMap<String , Vertex> h = new HashMap();
+        h.put("0" , new Vertex( new Point3D(1,1,0) , 0));
+        System.out.println(h.size());
+        System.out.println(h.get("0").getInfo());
+        System.out.println(h.containsKey("1"));
+    }
+}
